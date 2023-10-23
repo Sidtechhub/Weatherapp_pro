c89852d4bc3be4b654dcbc47b0de9f3f29c6aac7
Siddharth <129037073+Sidtechhub@users.noreply.github.com>
2023-10-23 07:07:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ def get_weatherdata():
     url="https://api.openweathermap.org/data/2.5/weather"
 
     params = {
-        'q' :requests.form.get("city"),
-        'appid' :requests.form.get("appid") ,
-        'units':requests.form.get("units")}
+        'q' :request.form.get("city"),
+        'appid' :request.form.get("appid") ,
+        'units':request.form.get("units")}
     response=requests.get(url,params=params)
     data=response.json()
     return f"data :{data}"
